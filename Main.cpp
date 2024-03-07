86a9d68a1f35d23850df8603a3193804fef05706
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-03-07 21:28:32 +0000

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,1 +1,21 @@
 include
+
+
+//motor rotate
+//move forwrad
+//while(1)
+/*
+  {deploy CNN;
+  grabbing input from camera feed
+  }
+  if(detected ==1)
+  {
+  }
+  else
+  {
+  }//deploy as interrupt
+*/
+/*
+  {detect RFID signal
+  }
+*/
