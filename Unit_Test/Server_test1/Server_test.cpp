cec5440e37513bc0277d114d99f8ae67df15cfeb
cyter23 <158228018+cyter23@users.noreply.github.com>
2024-07-28 21:41:16 +0100

    add comments

--- Unit_Test/Server_test1/Server_test.cpp
+++ Unit_Test/Server_test1/Server_test.cpp
@@ -9,17 +9,20 @@
 #include "Readmsg.h"
 #include "Trafficsig.h"
 
+// Test case for setting up the server
 TEST(ServerTest, SetupServer) {
-    Server server;
-    int server_fd;
-    struct sockaddr_in address;
-    int port = 5560;
+    Server server;           // Create a Server instance
+    int server_fd;           // Server file descriptor
+    struct sockaddr_in address; // Server address structure
+    int port = 5560;         // Port number
 
+    // Setup the server and expect the file descriptor to be greater than 0
     server.setupServer(server_fd, address, port);
-    EXPECT_GT(server_fd, 0);
+    EXPECT_GT(server_fd, 0); // Check if server_fd is greater than 0
 }
 
+// Main function to run all the tests
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS(); // Run all the tests
 }
