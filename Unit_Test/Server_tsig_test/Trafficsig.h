69bdc91036ed201238cf6ee3688d843d809c6636
cyter23 <158228018+cyter23@users.noreply.github.com>
2024-07-28 21:56:00 +0100

    add comments

--- Unit_Test/Server_tsig_test/Trafficsig.h
+++ Unit_Test/Server_tsig_test/Trafficsig.h
@@ -7,24 +7,29 @@
 #include <iostream>
 #include "pigpio.h"
 
-class TrafficsigTest;  // Forward declaration of the test class
+// Forward declaration of the test class
+class TrafficsigTest;
 
+// Trafficsig class inherits from CppThread to handle traffic signal control in a separate thread.
+// It manages traffic signals based on messages read by Readmsg and interacts with the Server to send signals.
 class Trafficsig : public CppThread {
 private:
-    Readmsg& rdmsg;
-    Server& ser;
-    bool traffic_signal;
-    int count = 0;
-    const int timer_id = 0;
+    Readmsg& rdmsg;          // Reference to the Readmsg instance
+    Server& ser;             // Reference to the Server instance
+    bool traffic_signal;     // Current state of the traffic signal
+    int count = 0;           // Counter for internal use
+    const int timer_id = 0;  // Timer ID for scheduling traffic signal changes
 
+    // Member function to handle traffic signals
     void trafficSignalHandler() {
-        ser.sendtrafsig(traffic_signal);
+        ser.sendtrafsig(traffic_signal);  // Send the current traffic signal state
         std::cout << "The traffic signal is " << traffic_signal << std::endl;
-        traffic_signal = !traffic_signal;
+        traffic_signal = !traffic_signal; // Toggle the traffic signal state
     }
 
+    // Overridden run method that contains the thread's execution code
     void run() {
-        traffic_signal = 0;
+        traffic_signal = 0; // Initialize the traffic signal state
         while (1) {
             if (strcmp(rdmsg.rdmsg, "Entered") == 0) {
                 for (int i = 0; i < 2; i++) {
@@ -37,25 +42,29 @@ private:
         }
     }
 
+    // Static function to handle traffic signals, called by the timer
     static void staticTrafficSignalHandler(void* userdata) {
         // Cast userdata to Trafficsig instance and call the member function
         Trafficsig* instance = static_cast<Trafficsig*>(userdata);
         instance->trafficSignalHandler();
     }
 
-    friend class TrafficsigTest;  // Grant access to the test class
+    // Grant access to the test class
+    friend class TrafficsigTest;
 
 public:
+    // Constructor initializing references to Readmsg and Server instances and setting initial traffic signal state
     Trafficsig(Readmsg& rdmsg, Server& server) : rdmsg(rdmsg), ser(server), traffic_signal(false) {}
     
+    // Public method to test the static traffic signal handler
     void testStaticTrafficSignalHandler() {
         staticTrafficSignalHandler(this);
     }
     
+    // Public method to get the current traffic signal state
     bool getTrafficSignal() const {
         return traffic_signal;
     }
-
 };
 
 #endif // TRAFFICSIG_H
