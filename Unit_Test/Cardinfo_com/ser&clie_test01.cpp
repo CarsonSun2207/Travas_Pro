b150ec54cceb5c6c58453453ae7df2fca7163916
cyter23 <158228018+cyter23@users.noreply.github.com>
2024-07-28 22:08:46 +0100

    add comments

--- Unit_Test/Cardinfo_com/ser&clie_test01.cpp
+++ Unit_Test/Cardinfo_com/ser&clie_test01.cpp
@@ -2,23 +2,28 @@
 #include <iostream>
 #include "client.h"
 
+// TestServer class inherits from Server and provides a test implementation for updateServerWithCardID
 class TestServer : public Server {
 public:
+    // Override the updateServerWithCardID method to provide a test implementation
     void updateServerWithCardID(const std::string& cardID) override {
         std::cout << "TestServer::updateServerWithCardID called with cardID: " << cardID << std::endl;
     }
 };
 
+// ClientServerTest class inherits from ::testing::Test and sets up the test environment
 class ClientServerTest : public ::testing::Test {
 protected:
-    TestServer testServer;
-    Client client;
+    TestServer testServer; // Instance of TestServer
+    Client client;         // Instance of Client
 
+    // Constructor initializes the client with the test server
     ClientServerTest() : client(&testServer) {
         std::cout << "ClientServerTest constructor" << std::endl;
     }
 };
 
+// Test case for simulating card reading
 TEST_F(ClientServerTest, SimulatesCardReading) {
     std::cout << "Running SimulatesCardReading test" << std::endl;
 
@@ -31,7 +36,8 @@ TEST_F(ClientServerTest, SimulatesCardReading) {
     std::cout << "After client.scanCard" << std::endl;
 }
 
+// Main function to run all the tests
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
-    return RUN_ALL_TESTS();
+    return RUN_ALL_TESTS(); // Run all the tests
 }
