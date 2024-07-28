40faa1396bc7c201d021eafc15b4d0ec720d0fd6
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-07-28 20:45:31 +0100

    Update motor.h

--- Client/motor.h
+++ Client/motor.h
@@ -3,17 +3,21 @@
 
 #include "pca9685.h"
 
+// Motor class definition to control motor movements using PCA9685 servo driver
 class Motor {
 private:
-    PCA9685& pca;
+    PCA9685& pca;  // Reference to a PCA9685 object for controlling PWM signals
 
 public:
+    // Constructor that takes a reference to a PCA9685 controller object
     Motor(PCA9685& pcaController);
-    void Forward();
-    void TurnLeft();
-    void TurnRight();
-    void Stop();
-    void Slow();
+
+    // Public member functions to control the motor
+    void Forward();   // Move the motor forward
+    void TurnLeft();  // Turn the motor left
+    void TurnRight(); // Turn the motor right
+    void Stop();      // Stop the motor
+    void Slow();      // Slow down the motor
 };
 
 #endif // MOTOR_H
