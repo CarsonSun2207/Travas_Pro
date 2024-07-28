50d7de86fa209d6d29190d4c02b5d46da186028a
cyter23 <158228018+cyter23@users.noreply.github.com>
2024-07-28 22:02:12 +0100

    add comments

--- Unit_Test/Cardinfo_com/CppThread.h
+++ Unit_Test/Cardinfo_com/CppThread.h
@@ -4,21 +4,28 @@
 
 #include <thread>
 
+// CppThread class provides a simple interface to start and join a thread.
+// Users of this class should inherit from it and implement the run method,
+// which contains the code that will be executed in the thread.
 class CppThread {
 public:
+    // Starts the thread by invoking the run method in a new thread.
     inline void start() {
         uthread = std::thread(&CppThread::run, this);
     }
 
+    // Joins the thread, blocking the calling thread until the thread finishes execution.
     inline void join() {
         uthread.join();
     }
 
 protected:
+    // Pure virtual function that must be overridden by derived classes.
+    // This function contains the code to be executed in the thread.
     virtual void run() = 0;
 
 private:
-    std::thread uthread;
+    std::thread uthread; // Internal thread object
 };
 
 #endif // CPPTHREAD_H
