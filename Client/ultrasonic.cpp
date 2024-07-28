c517a011d3d14aadc7f67742999e8c09646ce7cf
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-07-28 20:47:58 +0100

    Update ultrasonic.cpp

--- Client/ultrasonic.cpp
+++ Client/ultrasonic.cpp
@@ -1,30 +1,31 @@
-#ifndef ULTRASONIC_H
-#define ULTRASONIC_H
+#include "ultrasonic.h"
+#include "pigpio.h"
 
-#include "pca9685.h"
-#include <atomic>
+Ultrasonic::Ultrasonic(int triggerPin, int echoPin, PCA9685& controller) : trigPin(triggerPin), echoPin(echoPin), pca(controller) {
+    gpioSetMode(trigPin, PI_OUTPUT);
+    gpioSetMode(echoPin, PI_INPUT);
+    gpioWrite(trigPin, PI_LOW);
+    gpioSetAlertFuncEx(echoPin, echoCallback, this);
+}
 
-// Speed of sound in cm/s
-const float SOUND_SPEED = 34300;
+void Ultrasonic::triggerPulse() {
+    gpioWrite(trigPin, PI_HIGH);
+    gpioDelay(10);  // Use gpioDelay for accurate short delay
+    gpioWrite(trigPin, PI_LOW);
+}
 
-// Ultrasonic class to handle ultrasonic sensor operations
-class Ultrasonic {
-private:
-    int trigPin, echoPin;          // GPIO pins for the trigger and echo
-    PCA9685& pca;                  // Reference to the PCA9685 controller
-    std::atomic<float> lastDistance{0.0}; // Atomic variable to store the last measured distance
+void Ultrasonic::echoCallback(int gpio, int level, uint32_t tick, void *user1) {
+    static uint32_t startTick, endTick;
 
-    // Method to trigger the ultrasonic pulse
-    void triggerPulse();
-    // Static callback function to handle the echo response
-    static void echoCallback(int gpio, int level, uint32_t tick, void *user1);
+    if (level == 1) startTick = tick;
+    else if (level == 0) {
+        endTick = tick;
+        auto distance = ((endTick - startTick) / 1000000.0) * SOUND_SPEED / 2.0;
+        ((Ultrasonic*)user1)->lastDistance.store(distance);
+    }
+}
 
-public:
-    // Constructor to initialize the Ultrasonic object with trigger and echo pins and the PCA9685 controller
-    Ultrasonic(int triggerPin, int echoPin, PCA9685& controller);
-    
-    // Method to get the measured distance
-    float getDistance();
-};
-
-#endif // ULTRASONIC_H
+float Ultrasonic::getDistance() {
+    triggerPulse();
+    return lastDistance.load();
+}
