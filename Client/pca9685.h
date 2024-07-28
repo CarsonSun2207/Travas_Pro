4e7c617d850e033a2351d52339299eb56aaadbf6
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-07-28 20:46:09 +0100

    Update pca9685.h

--- Client/pca9685.h
+++ Client/pca9685.h
@@ -3,13 +3,19 @@
 
 #include "pigpio.h"
 
+// Class to interface with the PCA9685 PWM driver
 class PCA9685 {
 private:
-    int i2cHandle;
+    int i2cHandle;  // Handle for I2C communication
 
 public:
+    // Constructor that takes an I2C handle
     PCA9685(int handle);
+
+    // Method to set up the PCA9685
     void setup();
+
+    // Method to set the PWM signal for a specific channel
     void setPWM(int channel, int on, int off);
 };
 
