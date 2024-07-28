bd20fe8a4ce3f5e29e94bcc1affa26f0d73da771
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-07-28 20:45:12 +0100

    Update motor.cpp

--- Client/motor.cpp
+++ Client/motor.cpp
@@ -1,8 +1,11 @@
 #include "motor.h"
 
+// Constructor that initializes the Motor object with a reference to a PCA9685 controller
 Motor::Motor(PCA9685& pcaController) : pca(pcaController) {}
 
+// Method to move the motor forward
 void Motor::Forward() {
+    // Set PWM signals for forward movement on all motor channels
     pca.setPWM(0, 0, 0);
     pca.setPWM(1, 0, 850);
     pca.setPWM(3, 0, 0);
@@ -13,7 +16,9 @@ void Motor::Forward() {
     pca.setPWM(7, 0, 850);
 }
 
+// Method to turn the motor left
 void Motor::TurnLeft() {
+    // Set PWM signals for left turn on all motor channels
     pca.setPWM(0, 1024, 0);
     pca.setPWM(1, 0, 0);
     pca.setPWM(3, 1024, 0);
@@ -24,7 +29,9 @@ void Motor::TurnLeft() {
     pca.setPWM(7, 0, 1250);
 }
 
+// Method to turn the motor right
 void Motor::TurnRight() {
+    // Set PWM signals for right turn on all motor channels
     pca.setPWM(0, 0, 0);
     pca.setPWM(1, 0, 1250);
     pca.setPWM(3, 0, 0);
@@ -35,7 +42,9 @@ void Motor::TurnRight() {
     pca.setPWM(7, 0, 0);
 }
 
+// Method to stop the motor
 void Motor::Stop() {
+    // Set PWM signals to 0 for stopping all motor channels
     pca.setPWM(0, 0, 0);
     pca.setPWM(1, 0, 0);
     pca.setPWM(3, 0, 0);
@@ -46,7 +55,9 @@ void Motor::Stop() {
     pca.setPWM(7, 0, 0);
 }
 
+// Method to slow down the motor
 void Motor::Slow() {
+    // Set PWM signals for slower movement on all motor channels
     pca.setPWM(0, 0, 0);
     pca.setPWM(1, 0, 700);
     pca.setPWM(3, 0, 0);
