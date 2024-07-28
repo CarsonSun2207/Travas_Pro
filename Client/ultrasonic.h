7086d8ef825179facbcf8528637de41fbc2a6605
CarsonSun2207 <95834895+CarsonSun2207@users.noreply.github.com>
2024-07-28 20:47:38 +0100

    Update ultrasonic.h

--- Client/ultrasonic.h
+++ Client/ultrasonic.h
@@ -4,20 +4,26 @@
 #include "pca9685.h"
 #include <atomic>
 
+// Speed of sound in cm/s
 const float SOUND_SPEED = 34300;
 
+// Ultrasonic class to handle ultrasonic sensor operations
 class Ultrasonic {
 private:
-    int trigPin, echoPin;
-    PCA9685& pca;
-    std::atomic<float> lastDistance{0.0};
-    
+    int trigPin, echoPin;          // GPIO pins for the trigger and echo
+    PCA9685& pca;                  // Reference to the PCA9685 controller
+    std::atomic<float> lastDistance{0.0}; // Atomic variable to store the last measured distance
+
+    // Method to trigger the ultrasonic pulse
     void triggerPulse();
+    // Static callback function to handle the echo response
     static void echoCallback(int gpio, int level, uint32_t tick, void *user1);
 
 public:
-    
+    // Constructor to initialize the Ultrasonic object with trigger and echo pins and the PCA9685 controller
     Ultrasonic(int triggerPin, int echoPin, PCA9685& controller);
+    
+    // Method to get the measured distance
     float getDistance();
 };
 
